d6ecfd961f71c47c034fc2ac78c8b800ac0382e2
Alexander Letnikow <reclamme@gmail.com>
2017-07-31 16:41:50 +0300

    Add ActiveViewTrait

---
+++ src/Traits/ActiveViewTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Horat1us\Traits;
+
+use yii\base\InvalidCallException;
+
+/**
+ * Class ActiveViewTrait
+ * @package Horat1us\Traits
+ */
+trait ActiveViewTrait
+{
+    /**
+     * @param bool $runValidation
+     * @param null $attributeNames
+     * @see ActiveRecord::save()
+     */
+    public function save($runValidation = true, $attributeNames = NULL)
+    {
+        throw new InvalidCallException("ActiveView cannot be saved!");
+    }
+
+    /**
+     * @param bool $runValidation
+     * @param null $attributeNames
+     * @see ActiveRecord::update()
+     */
+    public function update($runValidation = true, $attributeNames = NULL)
+    {
+        throw new InvalidCallException("ActiveView cannot be updated!");
+    }
+}
