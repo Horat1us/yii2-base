69c8c927f5975470b1625a619d2bd7565400d869
Alexander Letnikow <reclamme@gmail.com>
2018-02-04 15:03:36 +0200

    Implement UrlHelper::append

---
+++ src/Helpers/UrlHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Horat1us\Yii\Helpers;
+
+/**
+ * Class UrlHelper
+ * @package Horat1us\Yii\Helpers
+ */
+class UrlHelper
+{
+    public static function append(string $url, array $params = []): string
+    {
+        $parts = parse_url($url);
+        parse_str($parts['query'], $existParams);
+
+        $mergedParams = array_merge($existParams, $params);
+        $parts['query'] = http_build_query($mergedParams);
+
+        return function_exists('http_build_url')
+            ? http_build_url($parts)
+            : $parts['scheme'] . '://' . $parts['host'] . $parts['path'] . '?' . $parts['query'];
+
+    }
+}
---
+++ tests/Helpers/UrlHelperTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Horat1us\Yii\Tests\Helpers;
+
+use Horat1us\Yii\Helpers\UrlHelper;
+use Horat1us\Yii\Tests\AbstractTestCase;
+
+/**
+ * Class UrlHelperTest
+ * @package Horat1us\Yii\Tests\Helpers
+ * @internal
+ */
+class UrlHelperTest extends AbstractTestCase
+{
+    public function testAppend()
+    {
+        $url = 'https://horatius.pro/abc/c?route=1&multiple%5B0%5D=1';
+        $processedUrl = UrlHelper::append($url, [
+            'action' => 'undefined',
+            'multiple' => [
+                2
+            ],
+        ]);
+        $this->assertEquals(
+            'https://horatius.pro/abc/c?route=1&multiple%5B0%5D=2&action=undefined',
+            $processedUrl
+        );
+    }
+}
